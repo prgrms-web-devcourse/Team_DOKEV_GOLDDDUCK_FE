fb11c6c05d231ba7a6c3dd397edf4d94465b7ffc
young-d <dodoga096@gmail.com>
2021-12-02 10:27:54 +0900

    feat: 색상, 아이콘, 글자크기 및 default margin 상수화

---
+++ src/utils/constants/sizes.ts
@@ -0,0 +1,13 @@
+interface IFontSize {
+  [size: string]: string
+}
+
+export const FONT_SIZES: IFontSize = {
+  micro: '12px',
+  small: '0.875rem',
+  base: '1rem',
+  medium: '1.125rem',
+  large: '1.5rem',
+}
+
+export const DEFAULT_MARGIN = '16px'
