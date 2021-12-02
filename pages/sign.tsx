35e153e4c441e68be1ed5b4ebac4ae7a2fedba9f
minsu-zip <alstn4814@gmail.com>
2021-12-02 16:20:02 +0900

    test : Button컴포넌트 테스트 코드

--- pages/sign.tsx
+++ pages/sign.tsx
@@ -1,5 +1,22 @@
+import MUIButton from '@components/MUIButton'
+
 const sign = () => {
-  return <div>로그인 페이지</div>
+  const onButtonClick = () => {
+    alert('안녕')
+  }
+
+  return (
+    <>
+      <MUIButton
+        onClick={onButtonClick}
+        color={'white'}
+        height={'90px'}
+        width={'80%'}
+        text={'테스트버튼'}
+        border-radius={'50px'}
+        background-color={'red'}></MUIButton>
+    </>
+  )
 }
 
 export default sign
