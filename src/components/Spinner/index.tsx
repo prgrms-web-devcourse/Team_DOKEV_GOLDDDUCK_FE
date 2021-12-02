765012614f45bb0b7433a9f3c2439b803884055f
young-d <dodoga096@gmail.com>
2021-12-02 19:32:37 +0900

    feat: Spinner 컴포넌트 구현

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { CircularProgress } from '@mui/material'
+
+const Spinner = (): React.ReactElement => {
+  return <CircularProgress sx={{ color: '#ffd700' }} />
+}
+
+export default Spinner
---
+++ src/stories/components/Spinner.stories.tsx
@@ -0,0 +1,10 @@
+import Spinner from '@components/Spinner'
+
+export default {
+  title: 'Component/Spinner',
+  component: Spinner,
+}
+
+export const Default = (args: any) => {
+  return <Spinner {...args} />
+}
