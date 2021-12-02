d03eff4fefebc1f92cf85118347edd9645488a6b
minsu-zip <alstn4814@gmail.com>
2021-12-03 01:39:29 +0900

    feat : Avatar 컴포넌트 및 storybook 구현

---
+++ src/components/MUIAvatar/index.tsx
@@ -0,0 +1,31 @@
+import React, { CSSProperties } from 'react'
+import Avatar from '@mui/material/Avatar'
+
+interface Props {
+  onClick?(): void
+  width?: string
+  height?: string
+  src?: string
+  style?: CSSProperties
+}
+
+const MUIAvatar: React.FC<Props> = ({
+  onClick,
+  width,
+  height,
+  src = '"/static/images/avatar/1.jpg"',
+  style,
+}) => {
+  return (
+    <>
+      <Avatar
+        onClick={onClick}
+        src={src}
+        sx={{ width, height }}
+        style={{ ...style }}
+      />
+    </>
+  )
+}
+
+export default MUIAvatar
---
+++ src/stories/Avtar.stories.tsx
@@ -0,0 +1,19 @@
+import Avtar from '@components/MUIAvatar'
+import { CSSProperties } from 'react'
+
+interface Props {
+  onClick?(): void
+  width?: string
+  height?: string
+  src?: string
+  style?: CSSProperties
+}
+
+export default {
+  title: 'Components/Button',
+  component: Avtar,
+}
+
+export const Default = (args: Props) => {
+  return <Avtar {...args}></Avtar>
+}
