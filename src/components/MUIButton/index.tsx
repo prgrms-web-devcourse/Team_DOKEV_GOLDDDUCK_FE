7661e6084dec3a5b743bff5b81288ef825f290fe
minsu-zip <alstn4814@gmail.com>
2021-12-02 16:21:07 +0900

    feat : MUIButton 컴포넌트 구현

---
+++ src/components/MUIButton/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Button from '@mui/material/Button'
+
+interface Props {
+  onClick?(): void
+  text?: string
+  color?: string
+  variant?: 'text' | 'contained' | 'outlined'
+  href?: string
+  height?: string
+  width?: string
+  backgroundColor?: string
+}
+
+const MUIButton: React.FC<Props> = ({
+  onClick,
+  text,
+  variant = 'contained',
+  href,
+  ...props
+}) => {
+  return (
+    <>
+      <Button
+        variant={variant}
+        href={href}
+        onClick={onClick}
+        style={{ ...props }}>
+        {text}
+      </Button>
+    </>
+  )
+}
+
+export default MUIButton
---
+++ src/stories/Buttion.stories.tsx
@@ -0,0 +1,21 @@
+import Button from '@components/MUIButton'
+
+interface Props {
+  onClick?(): void
+  text?: string
+  color?: string
+  variant?: 'text' | 'contained' | 'outlined'
+  href?: string
+  height?: string
+  width?: string
+  backgroundColor?: string
+}
+
+export default {
+  title: 'Components/Button',
+  component: Button,
+}
+
+export const Default = (args: Props) => {
+  return <Button {...args}></Button>
+}
