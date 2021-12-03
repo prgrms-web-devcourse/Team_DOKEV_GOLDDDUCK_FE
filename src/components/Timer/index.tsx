10cb5ce5b4d12ae9e09a7f10139efeb036d8a2f5
young-d <dodoga096@gmail.com>
2021-12-03 16:00:37 +0900

    chore: alias 적용 및 라인 정리

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, CSSProperties } from 'react'
-import { formatTimeNumber } from '../../utils/formatTimeNumber';
-import { COLORS } from '../../utils/constants/colors';
-import { FONT_SIZES } from '../../utils/constants/sizes';
+import { formatTimeNumber } from '@utils/formatTimeNumber'
+import { COLORS } from '@utils/constants/colors'
+import { FONT_SIZES } from '@utils/constants/sizes'
 
 interface ITimer {
   time: Date
@@ -10,33 +10,39 @@ interface ITimer {
   style?: CSSProperties
 }
 
-const SECOND = 1000;
-const MINUTE = SECOND * 60;
-const HOUR = MINUTE * 60;
+const SECOND = 1000
+const MINUTE = SECOND * 60
+const HOUR = MINUTE * 60
 
-const Timer = ({ time = new Date(), size = 'base', color = 'WHITE', ...style }: ITimer): React.ReactElement => {
-  const [restTime, setRestTime] = useState('');
-  let timer: NodeJS.Timer;
+const Timer = ({
+  time = new Date(),
+  size = 'BASE',
+  color = 'WHITE',
+  ...style
+}: ITimer): React.ReactElement => {
+  const [restTime, setRestTime] = useState('')
+  let timer: NodeJS.Timer
   const { [size]: fontSize } = FONT_SIZES
   const { [color]: fontColor } = COLORS
 
   const showRemaining = (): void => {
-    const now = new Date();
-    const distance = Number(time) - Number(now);
+    const now = new Date()
+    const distance = Number(time) - Number(now)
 
     if (distance < 0) {
-      clearInterval(timer);
-      return;
+      clearInterval(timer)
+
+      return
     }
-    
-    const hours = formatTimeNumber(Math.floor((distance) / HOUR))
+
+    const hours = formatTimeNumber(Math.floor(distance / HOUR))
     const minutes = formatTimeNumber(Math.floor((distance % HOUR) / MINUTE))
     const seconds = formatTimeNumber(Math.floor((distance % MINUTE) / SECOND))
     setRestTime(`${hours}:${minutes}:${seconds}`)
   }
 
   useEffect(() => {
-    timer = setInterval(showRemaining, 1000);
+    timer = setInterval(showRemaining, 1000)
   }, [showRemaining])
 
   return <div style={{ fontSize, color: fontColor, ...style }}>{restTime}</div>
