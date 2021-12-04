c3bcddd5d7e4f172c77e1b7c8fee7b6a7bb8b51b
minsu-zip <alstn4814@gmail.com>
2021-12-04 20:07:13 +0900

    test : Logo 컴포넌트 테스트 코드

---
+++ pages/testPagePartro.tsx
@@ -0,0 +1,12 @@
+import Logo from '@components/Logo'
+
+const testPagePartro = () => {
+  return (
+    <>
+      <Logo size="large"></Logo>
+      <Logo size="small"></Logo>
+    </>
+  )
+}
+
+export default testPagePartro
