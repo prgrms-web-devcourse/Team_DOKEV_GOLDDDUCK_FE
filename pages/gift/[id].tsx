dc5a01660bce39a8a2e38640d133a42b95b588ec
young-d <dodoga096@gmail.com>
2021-12-07 03:45:04 +0900

    feat: 선물 디테일 페이지 레이아웃 및 스타일

---
+++ pages/gift/[id].tsx
@@ -0,0 +1,141 @@
+import MUIButton from '@components/MUIButton'
+import Header from '@domains/Header'
+import styled from '@emotion/styled'
+import Image from '@components/Image'
+import Icon from '@components/Icon'
+import Text from '@components/Text'
+import { DEFAULT_MARGIN } from '@utils/constants/sizes'
+import giftImage from '/src/assets/gift_test.png'
+import { COLORS } from '@utils/constants/colors'
+import Switch from '@mui/material/Switch'
+import { useState } from 'react'
+
+const MUISwitch = () => {
+  const [checked, setChecked] = useState(true)
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked)
+  }
+
+  return (
+    <Switch
+      checked={checked}
+      onChange={handleChange}
+      inputProps={{ 'aria-label': 'controlled' }}
+    />
+  )
+}
+
+//Header, Icon, Text, text, switch, Text, Text, Text, Image, Button(download)
+const GiftDetailPage = () => {
+  return (
+    <>
+      <GiftContainerTop>
+        <Header />
+        <GiftInfo>
+          <Wrapper>
+            <Icon
+              name={'arrowBack'}
+              color={'WHITE'}
+              style={{ marginRight: 8 }}
+            />
+            <Text size={'BASE'} color={'WHITE'}>
+              {'시원한 아이스아메리카노'}
+            </Text>
+            <StyledSwitch>
+              <Text size={'MICRO'} color={'TEXT_GRAY_LIGHT'}>
+                {'사용 여부'}
+              </Text>
+              <MUISwitch />
+            </StyledSwitch>
+          </Wrapper>
+          <Wrapper>
+            <Text size={'MICRO'} color={'TEXT_GRAY_LIGHT'}>
+              {'받은 날짜'}
+            </Text>
+            <Text size={'MICRO'} color={'TEXT_GRAY_LIGHT'}>
+              {'2021년 12월 1일 12시 30분'}
+            </Text>
+            <Text
+              size={'MICRO'}
+              color={'TEXT_GRAY_LIGHT'}
+              style={{ marginLeft: 'auto' }}>
+              {'from.문타리'}
+            </Text>
+          </Wrapper>
+          <Image
+            src={giftImage.src}
+            width={'100%'}
+            height={'100%'}
+            style={{ marginBottom: DEFAULT_MARGIN }}
+          />
+          <Image
+            src={giftImage.src}
+            width={'100%'}
+            height={'100%'}
+            style={{ marginBottom: DEFAULT_MARGIN }}
+          />
+        </GiftInfo>
+      </GiftContainerTop>
+      <ContainerBottom>
+        <MUIButton
+          style={{
+            width: '100%',
+            borderRadius: 20,
+            backgroundColor: 'RED',
+          }}>
+          <a href={giftImage.src} download>
+            <Text color={'WHITE'} size={'BASE'}>
+              선물 저장
+            </Text>
+          </a>
+        </MUIButton>
+      </ContainerBottom>
+    </>
+  )
+}
+
+const GiftContainerTop = styled.div`
+  height: 100%;
+  overflow: scroll;
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+  &::-webkit-scrollbar {
+    display: none; /* Chrome, Safari, Opera*/
+  }
+`
+
+const GiftInfo = styled.div`
+  position: relative;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: ${DEFAULT_MARGIN};
+  background-color: inherit;
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  margin: 8px 0;
+  align-items: center;
+`
+
+const StyledSwitch = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+`
+
+const ContainerBottom = styled.div`
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  bottom: 0;
+  background-color: inherit;
+  padding: ${DEFAULT_MARGIN};
+`
+
+export default GiftDetailPage
