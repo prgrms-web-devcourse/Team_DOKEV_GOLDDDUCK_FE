612925f15ec082aef06f2ebf568613ce5ef96e28
Muntari29 <muntrock@gmail.com>
2021-12-07 21:09:44 +0900

    chore: 애니메이션 시간 변경 및 mp4 파일 교체

--- pages/index.tsx
+++ pages/index.tsx
@@ -82,7 +82,7 @@ const fadeIn = keyframes`
 `
 
 const Fade = styled.div`
-  animation: ${fadeIn} 3s 7s cubic-bezier(0.215, 0.61, 0.355, 1) both;
+  animation: ${fadeIn} 3s 6.6s cubic-bezier(0.215, 0.61, 0.355, 1) both;
 `
 
 export default main
