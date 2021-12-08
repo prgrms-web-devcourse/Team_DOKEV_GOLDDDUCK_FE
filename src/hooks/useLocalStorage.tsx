64d2d05fbbbeeabb2dfd5cf3b3b3fbbd681feb90
Muntari29 <muntrock@gmail.com>
2021-12-08 22:55:08 +0900

    chore: 불필요한 주석 삭제

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -35,38 +35,3 @@ const useLocalStorage = <T,>(
 }
 
 export default useLocalStorage
-
-// import { useState } from 'react'
-
-// const useLocalStorage = <T,>(
-//   key: string,
-//   initialValue: T,
-// ): [T, typeof setValue] => {
-//   // if (typeof window !== 'undefined'){}
-//   const [storedValue, setStoredValue] = useState(() => {
-//     try {
-//       console.log('111')
-//       const item = localStorage.getItem(key)
-
-//       return item ? JSON.parse(item) : initialValue
-//     } catch (e) {
-//       console.error(e)
-
-//       return initialValue
-//     }
-//   })
-
-//   const setValue = (value: T) => {
-//     console.log('22222')
-//     try {
-//       setStoredValue(value)
-//       localStorage.setItem(key, JSON.stringify(value))
-//     } catch (e) {
-//       console.error(e)
-//     }
-//   }
-
-//   return [storedValue, setValue]
-// }
-
-// export default useLocalStorage
