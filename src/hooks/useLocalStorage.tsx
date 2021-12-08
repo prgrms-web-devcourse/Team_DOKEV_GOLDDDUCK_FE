6108114473b980197aa5c1e373ba31e309968d1a
Muntari29 <muntrock@gmail.com>
2021-12-08 22:46:14 +0900

    fix: ReferenceError localstorage is not defined 에러 해결

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -5,23 +5,29 @@ const useLocalStorage = <T,>(
   initialValue: T,
 ): [T, typeof setValue] => {
   const [storedValue, setStoredValue] = useState(() => {
-    try {
-      const item = localStorage.getItem(key)
+    if (typeof window !== 'undefined') {
+      try {
+        const item = localStorage.getItem(key)
 
-      return item ? JSON.parse(item) : initialValue
-    } catch (e) {
-      console.error(e)
+        return item ? JSON.parse(item) : initialValue
+      } catch (e) {
+        console.error(e)
 
+        return initialValue
+      }
+    } else {
       return initialValue
     }
   })
 
   const setValue = (value: T) => {
-    try {
-      setStoredValue(value)
-      localStorage.setItem(key, JSON.stringify(value))
-    } catch (e) {
-      console.error(e)
+    if (typeof window !== 'undefined') {
+      try {
+        setStoredValue(value)
+        localStorage.setItem(key, JSON.stringify(value))
+      } catch (e) {
+        console.error(e)
+      }
     }
   }
 
@@ -29,3 +35,38 @@ const useLocalStorage = <T,>(
 }
 
 export default useLocalStorage
+
+// import { useState } from 'react'
+
+// const useLocalStorage = <T,>(
+//   key: string,
+//   initialValue: T,
+// ): [T, typeof setValue] => {
+//   // if (typeof window !== 'undefined'){}
+//   const [storedValue, setStoredValue] = useState(() => {
+//     try {
+//       console.log('111')
+//       const item = localStorage.getItem(key)
+
+//       return item ? JSON.parse(item) : initialValue
+//     } catch (e) {
+//       console.error(e)
+
+//       return initialValue
+//     }
+//   })
+
+//   const setValue = (value: T) => {
+//     console.log('22222')
+//     try {
+//       setStoredValue(value)
+//       localStorage.setItem(key, JSON.stringify(value))
+//     } catch (e) {
+//       console.error(e)
+//     }
+//   }
+
+//   return [storedValue, setValue]
+// }
+
+// export default useLocalStorage
