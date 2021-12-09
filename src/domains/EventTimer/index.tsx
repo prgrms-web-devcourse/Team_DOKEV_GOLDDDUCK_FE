e04871fe8c7412e4210bc7a568b032e3630aad92
minsu-zip <alstn4814@gmail.com>
2021-12-09 15:37:23 +0900

    feat : EventTimer(step2) 개발
    post(상위페이지)에서 상태값 컨트롤 + 레이아웃 디자인 수정

--- src/domains/EventTimer/index.tsx
+++ src/domains/EventTimer/index.tsx
@@ -5,51 +5,83 @@ import AdapterDateFns from '@mui/lab/AdapterDateFns'
 import LocalizationProvider from '@mui/lab/LocalizationProvider'
 import DateTimePicker from '@mui/lab/DateTimePicker'
 import Stack from '@mui/material/Stack'
+import Text from '@components/Text'
 
-const EventTimer = () => {
-  const [startvalue, setStartValue] = useState<Date | null>(new Date())
-  const [endvalue, setEndValue] = useState<Date | null>(new Date())
-  //const [startTimer, setStartTimer] = useState<Date | null>()
+interface Props {
+  startvalue: Date | null
+  endvalue: Date | null
+  handleStartTimer(e: Date | null): void
+  handleEndTimer(e: Date | null): void
+}
 
+const EventTimer = ({
+  startvalue,
+  endvalue,
+  handleStartTimer,
+  handleEndTimer,
+}: Props) => {
   return (
     <EventTimerContainer>
       <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <Stack spacing={3}>
-          <CustomDateTimePicker
-            className="MuiOutlinedInput-input MuiOutlinedInput-root"
-            renderInput={(params) => <TextField {...params} />}
-            label="시작 시간"
-            value={startvalue}
-            onChange={(newValue: Date | null) => {
-              setStartValue(newValue)
-            }}
-            minDateTime={new Date()}
-          />
-          <DateTimePicker
-            renderInput={(params) => <TextField {...params} />}
-            label="종료 시간"
-            value={endvalue}
-            onChange={(newValue) => {
-              setEndValue(newValue)
-            }}
-            minDate={startvalue}
-            minTime={startvalue}
-          />
-        </Stack>
+        <CustomStack spacing={8}>
+          <div>
+            <Text style={{ paddingBottom: '5px' }}>이벤트 시작 시간</Text>
+            <DateTimePicker
+              className="startvalue"
+              renderInput={(params) => <TextField {...params} />}
+              value={startvalue}
+              onChange={(newValue) => {
+                handleStartTimer(newValue)
+              }}
+              minDateTime={new Date()}
+            />
+          </div>
+
+          <div>
+            <Text style={{ paddingBottom: '5px' }}>이벤트 종료 시간</Text>
+            <DateTimePicker
+              className="endvalue"
+              renderInput={(params) => <TextField {...params} />}
+              value={endvalue}
+              onChange={(newValue) => {
+                handleEndTimer(newValue)
+              }}
+              minDateTime={startvalue}
+            />
+          </div>
+        </CustomStack>
       </LocalizationProvider>
     </EventTimerContainer>
   )
 }
 
 const EventTimerContainer = styled.div`
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin-top: 10%;
   display: flex;
   justify-content: center;
   align-items: center;
 `
-const CustomDateTimePicker = styled(DateTimePicker)(() => ({
+
+const CustomStack = styled(Stack)(() => ({
   '& .MuiOutlinedInput-input': {
-    backgroundColor: 'white',
-    color: 'red',
+    color: 'white',
+  },
+  '& .MuiInputLabel-root': { color: 'white' },
+  '& .MuiOutlinedInput-root.MuiInputBase-root.MuiInputBase-colorPrimary.Mui-error.MuiInputBase-formControl.MuiInputBase-adornedEnd.css-o9k5xi-MuiInputBase-root-MuiOutlinedInput-root':
+    { border: 'none' },
+  '& .MuiInputBase-colorPrimary.Mui-focused': { border: 'none' },
+  '& .MuiInputBase-colorPrimary': {
+    border: '1px solid white',
+    borderBlockColor: 'white',
+  },
+  '& .MuiSvgIcon-root.MuiSvgIcon-fontSizeMedium.css-i4bv87-MuiSvgIcon-root': {
+    color: 'white',
   },
 }))
+
 export default EventTimer
