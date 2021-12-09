01f2596c2262fb103098033a9d8db4566805207c
Muntari29 <muntrock@gmail.com>
2021-12-09 22:24:53 +0900

    chore : Random 비디오 추가

--- pages/random/[url].tsx
+++ pages/random/[url].tsx
@@ -2,68 +2,87 @@ import Header from '@domains/Header'
 import styled from '@emotion/styled'
 import { DEFAULT_MARGIN, FONT_SIZES } from '@utils/constants/sizes'
 import { COLORS } from '@utils/constants/colors'
+import TextHeader from '@domains/TimerHeader'
+import { useState } from 'react'
 
 const random = () => {
+  const [isStartVideo, setIsStartVideo] = useState(false)
+
+  const handleStartVideo = () => {
+    setIsStartVideo(true)
+  }
+
   return (
-    <FifoContainer>
+    <>
       <Header />
-      <Text>텍스트 컴포넌트</Text>
-      <Timer>타이머 컴포넌트</Timer>
-      <TimerText>텍스트 컴포넌트</TimerText>
+      <TextHeader />
       <GiftWrapper>
-        <VideoBox src="/video/gift.mp4" muted autoPlay />
+        {isStartVideo ? (
+          <VideoBox src="/video/Random.mp4" muted autoPlay />
+        ) : (
+          <ImageDiv onClick={handleStartVideo}>Orange</ImageDiv>
+        )}
+        {/* <VideoBox src="/video/3.mp4" muted autoPlay /> */}
       </GiftWrapper>
-    </FifoContainer>
+      {/* <CardContainer>
+        <Wrapper>
+          <Front>Oragne</Front>
+          <Back>Blue</Back>
+        </Wrapper>
+      </CardContainer> */}
+    </>
   )
 }
 
-const FifoContainer = styled.div`
-  width: 100%;
-  height: 100%;
-  background-color: blue;
-  display: flex;
-  flex-direction: column;
-  position: relative;
-  align-items: center;
-`
-
-const Text = styled.text`
-  font-size: ${FONT_SIZES.MEDIUM};
-  color: ${COLORS.WHITE};
-  background-color: gray;
-  position: absolute;
-  top: 120px;
-`
-
-const Timer = styled.div`
-  font-size: ${FONT_SIZES.LARGE};
-  color: ${COLORS.WHITE};
-  background-color: gray;
-  position: absolute;
-  top: 180px;
-`
-
-const TimerText = styled.text`
-  font-size: ${FONT_SIZES.SMALL};
-  color: ${COLORS.WHITE};
-  background-color: gray;
-  position: absolute;
-  top: 200px;
-`
-
 const GiftWrapper = styled.div`
   width: 100%;
+  height: 60%;
   position: absolute;
-  overflow: auto;
   bottom: 0;
-  height: 60vh;
-  padding-left: ${DEFAULT_MARGIN};
-  padding-right: ${DEFAULT_MARGIN};
-  background-color: greenyellow;
 `
 
 const VideoBox = styled.video`
   width: 100%;
 `
 
+const ImageDiv = styled.div`
+  display: inline-block;
+  width: 250px;
+  height: 250px;
+  background-color: orange;
+  transition-duration: 1s;
+`
+
+// const CardContainer = styled.div`
+//   perspective: 300px;
+// `
+
+// const Wrapper = styled.div`
+//   /* transition-duration: 1s;
+//   backface-visibility: hidden; */
+// `
+
+// const Front = styled.div`
+//   background-color: orange;
+//   transition-duration: 1s;
+//   backface-visibility: hidden;
+//   position: absolute;
+//   width: 200px;
+//   height: 200px;
+//   &:hover {
+//     transform: rotateY(180deg);
+//   }
+// `
+
+// const Back = styled.div`
+//   width: 200px;
+//   height: 200px;
+//   background-color: royalblue;
+//   transition-duration: 1s;
+//   backface-visibility: hidden;
+//   transform: rotateY(-180deg);
+//   &:hover {
+//     transform: rotateY(0);
+//   }
+// `
 export default random
