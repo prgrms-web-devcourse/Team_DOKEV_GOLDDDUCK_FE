94c8e628a70f0cb39c0116af5d6f1a07a67193f9
minsu-zip <alstn4814@gmail.com>
2021-12-10 18:29:02 +0900

    chore : 선물 icon 추가

--- src/utils/constants/icons.ts
+++ src/utils/constants/icons.ts
@@ -3,6 +3,7 @@ import { FiTrash2 } from 'react-icons/fi'
 import { BsPlusLg } from 'react-icons/bs'
 import { CgClose } from 'react-icons/cg'
 import { IconType } from 'react-icons/lib'
+import { AiFillGift } from 'react-icons/ai'
 
 interface IIconTypes {
   [iconName: string]: IconType
@@ -12,10 +13,12 @@ const $remove = FiTrash2
 const $arrowBack = MdArrowBack
 const $close = CgClose
 const $plus = BsPlusLg
+const $gift = AiFillGift
 
 export const ICON_TYPES: IIconTypes = {
   remove: $remove,
   arrowBack: $arrowBack,
   close: $close,
   plus: $plus,
+  gift: $gift,
 }
