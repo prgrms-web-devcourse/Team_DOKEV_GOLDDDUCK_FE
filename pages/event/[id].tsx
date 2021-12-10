e109fe7cec71cf17aa97c0d41bc526d1a9546ccf
young-d <dodoga096@gmail.com>
2021-12-10 14:25:42 +0900

    feat: 이벤트 타입, 종료 여부 렌더링, 이벤트 삭제 버튼 css 수정

--- pages/event/[id].tsx
+++ pages/event/[id].tsx
@@ -3,26 +3,51 @@ import Text from '@components/Text'
 import Header from '@domains/Header'
 import Icon from '@components/Icon'
 import MUIButton from '@components/MUIButton'
+import Modal from '@components/Modal'
 import { useRouter } from 'next/router'
 import styled from '@emotion/styled'
 import { DEFAULT_MARGIN } from '@utils/constants/sizes'
 import { COLORS } from '@utils/constants/colors'
-import { useCallback, useState } from 'react'
-import { css } from '@emotion/react'
 
-const MOCK_DATA = {
-  id: 1,
-  type: 'fifo',
-  cover: '1',
-  title: '데브코스 1기 수료를 축하합니다!',
-  code: 'einaldasdfsdfefdf',
-  open: new Date('Dec 24 2021 12:24:00 GMT+0900 (한국 표준시)'),
-  close: new Date('Jan 1 2022 1:1:1 GMT+0900 (한국 표준시)'),
-  isClosed: false,
-  winnerList: [
+const MOCK = {
+  eventId: 0,
+  title: '데브코스 수료를 축하합니다!',
+  code: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+  startAt: '2021-12-09T10:53:39.275Z',
+  endAt: '2021-12-09T10:53:39.274Z',
+  eventProgressStatus: 'READY',
+  giftChoiceType: 'FIFO',
+  mainTemplate: '3',
+  maxParticipantCount: 6,
+  gifts: [
     {
-      gift: '시원한 아이스아메리카노',
-      winners: [
+      id: 1,
+      category: '시원한 조개탕',
+      itemCount: 4,
+      giftItems: [
+        {
+          id: 0,
+          content: '조개탕은 2차로~',
+          giftType: 'TEXT',
+          used: false,
+        },
+      ],
+    },
+  ],
+  member: {
+    email: 'dogaga@email.com',
+    id: 10,
+    name: '도가가',
+    profileImage: '',
+    socialId: '도가가가',
+  },
+}
+
+const WINNER_MOCK = {
+  data: [
+    {
+      category: '시원한 아이스아메리카노',
+      users: [
         {
           id: 1,
           name: '파트로',
@@ -36,8 +61,8 @@ const MOCK_DATA = {
       ],
     },
     {
-      gift: '리아 친필사인',
-      winners: [
+      category: '리아 친필사인',
+      members: [
         {
           id: 3,
           name: '도가가',
@@ -56,8 +81,8 @@ const MOCK_DATA = {
       ],
     },
     {
-      gift: '기팍팍 응원메세지',
-      winners: [
+      category: '기팍팍 응원메세지',
+      members: [
         {
           id: 3,
           name: '도가가',
@@ -99,39 +124,47 @@ const MOCK_DATA = {
 }
 
 const EventPage = () => {
-  const eventData = MOCK_DATA
   const router = useRouter()
-  const EVENT_LINK = `http://localhost:3000/${eventData.type}/${eventData.code}`
+  const {
+    giftChoiceType: type,
+    code,
+    startAt: open,
+    endAt: close,
+    eventProgressStatus: status,
+  } = MOCK
+  const EVENT_LINK = `http://localhost:3000/${type.toLowerCase()}/${code}`
 
   const handleCopyUrl = () => {
     navigator.clipboard.writeText(EVENT_LINK)
   }
 
-  const formattedDate = (date: Date): Array<string> => {
+  const formattedDate = (date: string): Array<string> => {
+    const castingDate = new Date(date)
+
     return [
-      `${date.getFullYear()}년 ${date.getMonth()}월 ${date.getDate()}일`,
-      `${date.getHours()}시 ${date.getMinutes()}분 ${date.getSeconds()}초`,
+      `${castingDate.getFullYear()}년 ${castingDate.getMonth()}월 ${castingDate.getDate()}일`,
+      `${castingDate.getHours()}시 ${castingDate.getMinutes()}분 ${castingDate.getSeconds()}초`,
     ]
   }
 
   return (
-    <>
+    <Wrapper>
       <Header />
       <EventContainer>
         <Icon
           name="arrowBack"
           size="LARGE"
-          color="WHITE"
           onIconClick={() => router.push('/mypage?tab=event')}
           style={{
             position: 'absolute',
             top: 0,
             left: 0,
+            marginLeft: 16,
           }}
         />
         <CoverImage
           style={{
-            backgroundImage: `url(/cover/cover${eventData.cover}.png)`,
+            backgroundImage: `url(/cover/cover${MOCK.mainTemplate}.png)`,
           }}>
           <Text
             color="BLACK"
@@ -141,99 +174,136 @@ const EventPage = () => {
               wordBreak: 'keep-all',
               lineHeight: '1.5',
             }}>
-            {eventData.title}
+            {MOCK.title}
           </Text>
           <MUIButton onClick={handleCopyUrl} style={{ ...copyBtnStyle }}>
             링크 복사
           </MUIButton>
+          <Text color="TEXT_GRAY_DARK">
+            {`${type === 'FIFO' ? '#선착순' : '#랜덤'} ${
+              status === 'READY'
+                ? '#준비중'
+                : status === 'RUNNING'
+                ? '#진행중'
+                : '#종료됨'
+            }`}
+          </Text>
+          <RemoveButton>
+            <Icon
+              name="remove"
+              size="MEDIUM"
+              style={{ margin: '10px 0 0 10px' }}
+              onIconClick={() => {
+                confirm(
+                  '이벤트를 삭제하시겠습니까?\n삭제한 이벤트는 종료 처리되고 더 이상 확인할 수 없습니다.',
+                ) && alert('삭제되었습니다')
+              }}
+            />
+          </RemoveButton>
         </CoverImage>
-        <EventTimeInfo>
-          <TimeWrapper>
+        <EventTimeSection>
+          <TimeInfo>
             <Text>오픈</Text>
             <Text
               color="TEXT_GRAY_LIGHT"
               size="MICRO"
-              style={{ textAlign: 'center', marginTop: 16, marginBottom: 4 }}>
-              {formattedDate(eventData.open)[0]}
+              style={{ marginTop: 16, marginBottom: 4 }}>
+              {formattedDate(open)[0]}
             </Text>
-            <Text
-              size="MICRO"
-              color="TEXT_GRAY_LIGHT"
-              style={{ textAlign: 'center' }}>
-              {formattedDate(eventData.open)[1]}
+            <Text size="MICRO" color="TEXT_GRAY_LIGHT">
+              {formattedDate(open)[1]}
             </Text>
-          </TimeWrapper>
+          </TimeInfo>
           <Divider />
-          <TimeWrapper>
+          <TimeInfo>
             <Text>종료</Text>
             <Text
               color="TEXT_GRAY_LIGHT"
               size="MICRO"
-              style={{ textAlign: 'center', marginTop: 16, marginBottom: 4 }}>
-              {formattedDate(eventData.close)[0]}
+              style={{ marginTop: 16, marginBottom: 4 }}>
+              {formattedDate(close)[0]}
             </Text>
-            <Text
-              size="MICRO"
-              color="TEXT_GRAY_LIGHT"
-              style={{ textAlign: 'center' }}>
-              {formattedDate(eventData.close)[1]}
+            <Text size="MICRO" color="TEXT_GRAY_LIGHT">
+              {formattedDate(close)[1]}
             </Text>
-          </TimeWrapper>
-        </EventTimeInfo>
-        <MUIButton
-          variant="outlined"
-          onClick={() => {
-            confirm(
-              '이벤트를 삭제하시겠습니까?\n삭제한 이벤트는 종료 처리되고 더 이상 확인할 수 없습니다.',
-            ) && alert('삭제되었습니다')
-          }}
-          sx={{
-            borderColor: COLORS.TEXT_GRAY_DARK,
-            color: COLORS.TEXT_GRAY_DARK,
-            '&:hover': {
-              borderColor: COLORS.RED,
-              color: COLORS.RED,
-            },
-          }}>
-          이벤트 삭제
-        </MUIButton>
+          </TimeInfo>
+        </EventTimeSection>
+        <WinnerSection>
+          <Text>당첨자 확인</Text>
+          <Icon name="pointDown" size="MEDIUM" />
+        </WinnerSection>
       </EventContainer>
-    </>
+      <Modal>
+        <MUIButton style={{ ...winnerBtnStyle }}>클릭</MUIButton>
+        <Text color="WHITE">당첨자 목록</Text>
+      </Modal>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: ${COLORS.BLACK};
+  height: 100%;
+`
 
 const copyBtnStyle: React.CSSProperties = {
   backgroundColor: COLORS.RED,
   borderRadius: 20,
+  lineHeight: 2,
+}
+
+const winnerBtnStyle: React.CSSProperties = {
+  position: 'relative',
+  bottom: 0,
+  backgroundColor: COLORS.GREEN,
+  borderRadius: 20,
+  lineHeight: 2,
+  margin: `${DEFAULT_MARGIN} 40%`,
 }
 
 const EventContainer = styled.div`
-  margin: ${DEFAULT_MARGIN};
   position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
+  margin-top: ${DEFAULT_MARGIN};
   background-color: inherit;
 `
 const CoverImage = styled.div`
-  width: 300px;
+  position: relative;
+  width: 70%;
   height: 400px;
   background-repeat: no-repeat;
-  background-size: contain;
+  padding: 40px;
+  background-size: cover;
+  border-radius: 20px;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
   align-items: center;
 `
 
-const EventTimeInfo = styled.div`
-  display: flex;
-  margin: ${DEFAULT_MARGIN} 0;
-  height: 80px;
-  width: 300px;
+const RemoveButton = styled.div`
+  position: absolute;
+  bottom: -1px;
+  right: -1px;
+  background-color: ${COLORS.BLACK};
+  border-radius: 50px 0 0 0;
 `
 
-const TimeWrapper = styled.section`
+const EventTimeSection = styled.section`
+  display: flex;
+  margin: 10% 0;
+  height: 80px;
+  width: 80%;
+`
+
+const TimeInfo = styled.div`
   color: ${COLORS.WHITE};
   width: 50%;
   height: 100%;
@@ -247,6 +317,16 @@ const Divider = styled.div`
   width: 1px;
   height: 100%;
   background-color: ${COLORS.TEXT_GRAY_DARK};
+`
+
+const WinnerSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  height: 50px;
+  margin: 10% 0;
+  background-color: inherit;
 `
 
 export default EventPage
--- src/utils/constants/icons.ts
+++ src/utils/constants/icons.ts
@@ -1,5 +1,6 @@
 import { MdArrowBack } from 'react-icons/md'
 import { FaRegHandPointDown } from 'react-icons/fa'
+import { AiOutlineDelete } from 'react-icons/ai'
 import { BsPlusLg } from 'react-icons/bs'
 import { CgClose } from 'react-icons/cg'
 import { IconType } from 'react-icons/lib'
@@ -12,10 +13,12 @@ const $pointDown = FaRegHandPointDown
 const $arrowBack = MdArrowBack
 const $close = CgClose
 const $plus = BsPlusLg
+const $remove = AiOutlineDelete
 
 export const ICON_TYPES: IIconTypes = {
-  remove: $pointDown,
+  pointDown: $pointDown,
   arrowBack: $arrowBack,
   close: $close,
   plus: $plus,
+  remove: $remove,
 }
