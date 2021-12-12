774ebf3735f366139c969e8fc99a271cbeb52fd3
young-d <dodoga096@gmail.com>
2021-12-12 18:51:58 +0900

    feat: 체크박스 컴포넌트 및 체크박스 리스트 구현

---
+++ src/stories/components/Checkbox.stories.tsx
@@ -0,0 +1,40 @@
+import Checkbox from '@components/Checkbox'
+import { FONT_SIZES } from '@utils/constants/sizes'
+
+interface ICheckbox {
+  key: string
+  name: string
+  size?: 'MICRO' | 'SMALL' | 'BASE' | 'MEDIUM' | 'LARGE'
+  checked: boolean
+  onClick: React.MouseEventHandler<HTMLInputElement>
+}
+
+export default {
+  title: 'Components/Checkbox',
+  component: Checkbox,
+  argTypes: {
+    name: {
+      control: 'text',
+    },
+    size: {
+      options: Object.keys(FONT_SIZES),
+      control: 'select',
+      defaultValue: 'BASE',
+    },
+    checked: {
+      control: 'boolean',
+      defaultValue: false,
+    },
+    key: {
+      control: 'text',
+    },
+  },
+}
+
+export const Default = (args: ICheckbox) => {
+  return (
+    <div style={{ backgroundColor: 'black', padding: 20 }}>
+      <Checkbox {...args} />
+    </div>
+  )
+}
