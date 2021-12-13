f520e013c7b34d70246ed079136fdb3d236597fb
young-d <dodoga096@gmail.com>
2021-12-14 03:36:47 +0900

    feat: 이벤트 목록, 이벤트 아이템 데이터 타입

--- src/types/event.d.ts
+++ src/types/event.d.ts
@@ -1,3 +1,32 @@
-export declare type EVENT_TEMPLATE = '1' | '2' | '3' | '4' | '5' | '6'
+export declare type EVENT_TEMPLATE =
+  | 'template1'
+  | 'template2'
+  | 'template3'
+  | 'template4'
+  | 'template5'
+  | 'template6'
 export declare type EVENT_STATUS = 'READY' | 'RUNNING' | 'CLOSED'
 export declare type EVENT_TYPE = 'FIFO' | 'RANDOM'
+
+export interface IEventList {
+  pagination: {
+    currentPage: number
+    offset: number
+    size: number
+    totalElements: number
+    totalPages: number
+  }
+  simpleEventList: Array<IEventItem>
+}
+export interface IEventItem {
+  code: string
+  createdAt: string
+  endAt: string
+  eventId: number
+  eventProgressStatus: EVENT_STATUS
+  giftChoiceType: EVENT_TYPE
+  mainTemplate: EVENT_TEMPLATE
+  maxParticipantCount: number
+  startAt: string
+  title: string
+}
