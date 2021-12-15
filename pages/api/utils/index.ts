104a010d8479fab57f70fe8f4b15de6ebfa7dd39
minsu-zip <alstn4814@gmail.com>
2021-12-15 22:30:21 +0900

    feat: multipartFrom 인스턴스 모듈 생성

--- pages/api/utils/index.ts
+++ pages/api/utils/index.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosRequestConfig } from 'axios'
 
-const BASE_URL = 'http://maenguin.iptime.org:8080/'
+const BASE_URL =
+  'http://ec2-3-36-59-242.ap-northeast-2.compute.amazonaws.com:8080/'
 
 const axiosAuthApi = (
   url: string,
@@ -19,4 +20,25 @@ const axiosAuthApi = (
   return instance
 }
 
+const axiosMultipart = (
+  url: string,
+  options?: AxiosRequestConfig<object> | undefined,
+) => {
+  const token =
+    typeof window !== 'undefined' ? localStorage.getItem('token') : ''
+  const userToken = token ? token.replace(/\"/gi, '') : ''
+
+  const instance = axios.create({
+    baseURL: url,
+    headers: {
+      'Content-Type': 'multipart/form-data',
+      'X-GOLDDDUCK-AUTH': `Bearer ${userToken}`,
+    },
+    ...options,
+  })
+
+  return instance
+}
+
 export const authInstance = axiosAuthApi(BASE_URL)
+export const multipart = axiosMultipart(BASE_URL)
