d440314ac8fa56770ef4b6a9782a0d12fc930e36
minsu-zip <alstn4814@gmail.com>
2021-12-21 16:11:14 +0900

    chore: 선착순 설명 텍스트 수정

--- pages/fifo/[url].tsx
+++ pages/fifo/[url].tsx
@@ -164,7 +164,7 @@ const fifo = (): JSX.Element => {
         <TimerHeader
           eventStart={new Date(eventData.startAt)}
           eventMaster={eventData.member.name}
-          message="눈치게임! 기회는 단 한 번뿐!"
+          message="선택의 기회는 단 한 번뿐!"
         />
         <GiftWrapper>
           {eventData.gifts.map(({ id, category, itemCount }: Igifts, index) => (
